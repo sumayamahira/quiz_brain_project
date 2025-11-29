90705842a79bda692e48d7fedb3340490cf902d5
Mahi <coder.sumayamahira@gmail.com>
2025-11-30 01:45:02 +0600

    add extensive amount of data

--- data.py
+++ data.py
@@ -5,9 +5,7 @@ question_data = [
         "difficulty": "medium",
         "question": "The HTML5 standard was published in 2014.",
         "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
+        "incorrect_answers": ["False"]
     },
     {
         "category": "Science: Computers",
@@ -15,9 +13,7 @@ question_data = [
         "difficulty": "medium",
         "question": "The first computer bug was formed by faulty wires.",
         "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
+        "incorrect_answers": ["True"]
     },
     {
         "category": "Science: Computers",
@@ -25,9 +21,7 @@ question_data = [
         "difficulty": "medium",
         "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
         "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
+        "incorrect_answers": ["True"]
     },
     {
         "category": "Science: Computers",
@@ -35,9 +29,7 @@ question_data = [
         "difficulty": "medium",
         "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
         "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
+        "incorrect_answers": ["True"]
     },
     {
         "category": "Science: Computers",
@@ -45,9 +37,7 @@ question_data = [
         "difficulty": "easy",
         "question": "Linus Torvalds created Linux and Git.",
         "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
+        "incorrect_answers": ["False"]
     },
     {
         "category": "Science: Computers",
@@ -55,9 +45,7 @@ question_data = [
         "difficulty": "easy",
         "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
         "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
+        "incorrect_answers": ["True"]
     },
     {
         "category": "Science: Computers",
@@ -65,9 +53,7 @@ question_data = [
         "difficulty": "medium",
         "question": "AMD created the first consumer 64-bit processor.",
         "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
+        "incorrect_answers": ["False"]
     },
     {
         "category": "Science: Computers",
@@ -75,9 +61,7 @@ question_data = [
         "difficulty": "easy",
         "question": "'HTML' stands for Hypertext Markup Language.",
         "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
+        "incorrect_answers": ["False"]
     },
     {
         "category": "Science: Computers",
@@ -85,9 +69,7 @@ question_data = [
         "difficulty": "easy",
         "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
         "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
+        "incorrect_answers": ["False"]
     },
     {
         "category": "Science: Computers",
@@ -95,9 +77,215 @@ question_data = [
         "difficulty": "hard",
         "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
         "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    }
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Politics",
+        "question": "Former president Theodore Roosevelt (1900-1908) ran for another term under the Progressive Party in 1912.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+
+    {"type": "boolean",
+     "difficulty": "medium",
+     "category": "Sports",
+     "question": "ATP tennis hosted several tournaments on carpet court before being replaced to reduce injuries.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "In the Mortal Kombat series the \"Deadly Alliance\" consists of Shang Tsung and Quan Chi",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "Linux was first created as an alternative to Windows XP.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "In \"Need for Speed: Porsche Unleashed\", the player can only drive cars manufactured by Porsche.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+     "question": "Adolf Hitler was born in Australia.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "History",
+     "question": "Adolf Hitler was a german soldier in World War I.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Cartoon & Animations",
+     "question": "In the \"Shrek\" film franchise, Donkey is played by Eddie Murphy.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "History",
+     "question": "United States President Ronald Reagan was the first president to appoint a woman to the Supreme Court.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Geography",
+     "question": "Vietnam is the only country in the world that starts with V.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Mythology",
+     "question": "Rannamaari was a sea demon that haunted the people of the Maldives and had to be appeased monthly with the sacrifice of a virgin girl.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+     "question": "Slovakia is a member of European Union.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Video Games",
+     "question": "Fortnite was originally intended to be a horror game.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "In Dead Space 2, the 'Hand Cannon' is a large foam finger.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Entertainment: Video Games",
+     "question": "Unturned originally started as a Roblox game.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "General Knowledge",
+     "question": "Spoon theory is a theory, utilizing 'Spoons' as a metaphor for energy they can use in a day.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Geography",
+     "question": "Greenland is covered with grass and Iceland covered with ice.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Entertainment: Music",
+     "question": "Pete Townshend's solo album, \"White City: A Novel\", is set in the metropolitan area of Chicago.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Books",
+     "question": "\"Green Eggs and Ham\" consists of only 50 different words.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Science & Nature",
+     "question": "The shape of the Earth is a perfect sphere.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Science & Nature",
+     "question": "Frederick Banting and John Macleod won a Nobel Prize for their contributions to the scientific fields of quantum mechanics.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "History",
+     "question": "Augustus was the cousin of Julius Caesar.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Science & Nature",
+     "question": "It was once believed that injecting shark cartilage into people would prevent them from contracting cancer.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Science & Nature",
+     "question": "A person can get sunburned on a cloudy day.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Science & Nature",
+     "question": "The Neanderthals were a direct ancestor of modern humans.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Television",
+     "question": "Bob Ross was a US Air Force pilot.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Film",
+     "question": "The movie \"The Nightmare before Christmas\" was all done with physical objects.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "It's not possible to format a write-protected DVD-R Hard Disk.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Science & Nature",
+     "question": "A defibrillator is used to start up a heartbeat once a heart has stopped beating.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+     "question": "The logo for Snapchat is a Bell.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Film",
+     "question": "In Alfred Hitchcock's film 'Psycho' it is said he used chocolate syrup to simulate the blood in the famous shower scene.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Gadgets",
+     "question": "The Western Electric Model 500 telephone uses tone dialing to dial phone numbers.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "Wilt Chamberlain scored his infamous 100-point-game against the New York Knicks in 1962.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Video Games",
+     "question": "Hidden in the files for \"Mario Kart Arcade GP\" is a picture of the Beslan school hostage crisis.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "History",
+     "question": "The USS Missouri (BB-63) last served in the Korean War.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "Niko Bellic is the protagonist of Grand Theft Auto IV.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+     "question": "Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 inside swimming pools and drinking water.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Entertainment: Video Games",
+     "question": "Super Mario and Paarthurnax (Skyrim) are voiced by the same actor.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Entertainment: Music",
+     "question": "Myoui Mina, member of Korean girl group TWICE, was born in South Korea, despite being Japanese.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+     "question": "The disease cancer in itself is not contagious.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Entertainment: Video Games",
+     "question": "In Undertale, having a \"Fun Value\" set to 56-57 will play the \"Wrong Number Song Call\".",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Books",
+     "question": "\"Elementary, my dear Watson\" is a phrase that is never truly said within the Conan Doyle books of Sherlock Holmes.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+     "question": "Cucumbers are usually more than 90% water.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Music",
+     "question": "Scottish producer Calvin Harris is from the town of Dumfries, Scotland.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Television",
+     "question": "In the animated series Futurama, the character Phllip J. Fry is his own grandfather.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "hard", "category": "Science: Mathematics",
+     "question": "If you could fold a piece of paper in half 50 times, its thickness will be 3/4th the distance from the Earth to the Sun.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "Tetris is the #1 best-selling video game of all-time.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Science & Nature",
+     "question": "Hippopotomonstrosesquippedaliophobia is the irrational fear of long words.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+     "question": "Nutella is produced by the German company Ferrero.",
+     "correct_answer": "False", "incorrect_answers": ["True"]}
 ]
- 
